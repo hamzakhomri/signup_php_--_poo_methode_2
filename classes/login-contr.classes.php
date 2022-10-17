223c05a0074886cf028f0e3c330aa9e3471cca0c
hamzakhomri <hamza.khomri36@gmail.com>
2022-10-17 15:59:50 +0200

    SIGN-UP DONE

---
+++ classes/login-contr.classes.php
@@ -0,0 +1,46 @@
+<?php
+
+class SignupContr extends signup{
+
+    private $adresse;
+    private $password;
+
+    public function __construct($adresse,$password){
+
+        $this->adresse=$adresse;
+        $this->password=$password;
+
+    }
+
+    public function loginUser(){
+
+        if($this->emptyInput() == false){
+            //echo "Invalid Input";
+            $parent = dirname($_SERVER['REQUEST_URI']);
+            header("Location: $parent/../index.php?error=emptyInput");
+            // header ("location : /index.php?error=emptyInput");
+            exit();
+        }
+
+        $this->getUser($this->adresse,$this->password);
+
+    }
+    
+    private function emptyInput(){
+
+       $result;
+        if( empty($this->adresse) || empty( $this->password))
+        {
+            $result =false;
+        }	
+
+        else
+        {
+            $result = true;
+        }
+
+        return $result;
+    }
+}
+
+?>
